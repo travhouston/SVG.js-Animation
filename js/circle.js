06556153f84cb6b035ef253dc217bc2d4e2ea4d7
Travis Houston <tjhouston14@gmail.com>
2019-02-19 11:40:31 +0000

    Update circle.js

--- js/circle.js
+++ js/circle.js
@@ -2,7 +2,6 @@ var draw = SVG('drawing').size(1000, 1000)
 
 var circ = draw.circle(100).fill('dodgerblue')
 
-
   circ.animate().after(function() {
     this.animate().move(500)
     this.animate().move(500,500)
